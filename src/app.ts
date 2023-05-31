9a898b86b21cacb2edc755f888e0f6d80ceb70a2
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2023-05-31 16:45:44 -0300

    Integracao com Prisma ORM - PostgreSQL com Docker, Utilizando o Doker Compose, Criando schema do Prisma e Relacionamentos entre tabelas

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export const app = fastify()
+
+prisma.user.create({
+  data: {
+    name: 'Fernando',
+    email: 'fernando@email.com',
+  },
+})
