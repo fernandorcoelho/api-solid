256f462026d121bfaabd10b5c61a959288e3577d
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2023-06-01 17:46:15 -0300

    Implementing SOLID

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-prisma.user.create({
-  data: {
-    name: 'Fernando',
-    email: 'fernando@email.com',
-  },
-})
+app.register(appRoutes)
